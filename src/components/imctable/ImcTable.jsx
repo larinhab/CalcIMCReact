fff8bfc38e0ac73ddfd002668d2f7019da6269e3
larinhab <larabviana@outlook.com>
2024-04-03 00:32:16 -0300

    :new: a few refactoring

--- src/components/imctable/ImcTable.jsx
+++ src/components/imctable/ImcTable.jsx
@@ -1,29 +1,38 @@
 import './ImcTable.css'
+import Button from '../button/button'
 
-function ImcTable() {
+function ImcTable({ data, imc, info, infoClass, resetCalc}) {
   return (
     <div id='result-container'>
-        <p id='imc-number'>Seu IMC:</p>
+       
+        <p id='imc-number'>IMC:
+            <span className={ infoClass }> { imc } </span>
+        </p>
         
-        <p id='imc-info'>Situação atual:</p>
+        <p id='imc-info'>Health Risk:
+            <span className={ infoClass }> { info } </span>
+        </p>
 
-        <h3>Confira sua classificação:</h3>
+        <h3> Classification:</h3>
         <div id='imc-table'>
             <div className='table-header'>
                 <h4>IMC</h4>
-                <h4>Classificação</h4>
-                <h4>Obesidade</h4>
+                <h4>Classification</h4>
+                <h4>Obesity</h4>
             </div>
             {data.map((item) => (
                 <div className='table-data'
-                    key={item.info}>
+                    key= {item.info}>
                         <p>{item.classification}</p>
                         <p>{item.info}</p>
                         <p>{item.obesity}</p>
                     </div>
             ))}
         </div>
-        <Button id="btn-back" text="Voltar"></Button>
+        <Button 
+            id="btn-back" 
+            text="Return" 
+            action={ resetCalc }></Button>
         </div>
   )
 }
