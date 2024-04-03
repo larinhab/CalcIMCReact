9125ecd348d87e4a459f5390f07f964b81b59a76
larinhab <larabviana@outlook.com>
2024-04-02 23:07:26 -0300

    :new: add data array

---
+++ src/data/data.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+      min: 0,
+      max: 18.4,
+      classification: "Less then 18,5",
+      info: "Thinness",
+      obesity: "0",
+      infoClass: "medium"
+    },
+    {
+      min: 18.5,
+      max: 24.9,
+      classification: "Between 18,5 e 24,9",
+      info: "Normal",
+      obesity: "0",
+      infoClass: "good"
+    },
+    {
+      min: 25,
+      max: 29.9,
+      classification: "Entre 25,0 e 29,9",
+      info: "Overweight",
+      obesity: "I",
+      infoClass: "medium"
+    },
+    {
+      min: 30,
+      max: 39.9,
+      classification: "Entre 30,0 e 39,9",
+      info: "Obesity",
+      obesity: "II",
+      infoClass: "medium"
+    },
+    {
+      min: 40,
+      max: 99,
+      classification: "Bigger then 40,0",
+      info: "Severe Obesity",
+      obesity: "III",
+      infoClass: "medium"
+    },
+  ];
+
+  export default data
