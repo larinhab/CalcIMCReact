96f0773ce291afcdd7311a59f4714206addbd43d
larinhab <larabviana@outlook.com>
2024-04-02 23:37:04 -0300

    :bug: fix name components. add css style

--- src/components/imctable/ImcTable.jsx
+++ src/components/imctable/ImcTable.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import './ImcTable.css'
 
 function ImcTable() {
   return (
-    <div>ImcTable</div>
+    <div id='result-container'>
+        <p id='imc-number'>Seu IMC:</p>
+        
+        <p id='imc-info'>Situação atual:</p>
+
+        <h3>Confira sua classificação:</h3>
+        <div id='imc-table'>
+            <div className='table-header'>
+                <h4>IMC</h4>
+                <h4>Classificação</h4>
+                <h4>Obesidade</h4>
+            </div>
+            {data.map((item) => (
+                <div className='table-data'
+                    key={item.info}>
+                        <p>{item.classification}</p>
+                        <p>{item.info}</p>
+                        <p>{item.obesity}</p>
+                    </div>
+            ))}
+        </div>
+        <Button id="btn-back" text="Voltar"></Button>
+        </div>
   )
 }
 
